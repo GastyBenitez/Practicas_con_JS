ab39694e12ed89dad432466e64c34f82d86be7d0
gaston benitez <gastonbenitez0207@gmail.com>
2021-12-07 21:39:25 -0300

    practicas con js

---
+++ app.js
@@ -0,0 +1,102 @@
+//Ejercicio 0
+//const nombre = prompt ("¿Cuál es tú nombre?");
+//alert ("Hola " + nombre);
+
+
+
+//const apellido = prompt ("¿Cuál es tu apellido?");
+//alert ("Hola " + nombre + " " + apellido);
+
+
+//Ejercicio 1
+//const primerNumero = prompt ("Ingrese un numero");
+//const segundoNumero = prompt ("Ingrese otro numero");
+//const suma = parseInt(primerNumero) + parseInt(segundoNumero);
+//alert ( "El resultado es " + suma);
+
+//Ejercicio 2
+//const actualYear = prompt("Por favor ingrese el año acual");
+//const edadActual = prompt("¿Cuantos años tienes?");
+//const bornDate = parseInt(actualYear) - parseInt(edadActual);
+
+//alert (bornDate);
+
+//Ejercicio 3
+//const numeroDeCuenta = "2335/445";
+//const saldoInicial = 1500;
+
+//const montoDeRetiro = prompt("Por favor ingrese cuanto dinero desea retirar");
+
+//const balance = saldoInicial - parseInt(montoDeRetiro);
+//alert("el saldo de la cuenta numero: " + numeroDeCuenta + " " + "es de " + balance);
+
+//Ejercicio 4
+//const notaUno = prompt("Ingrese una nota");
+//const notaDos = prompt("Ingrese otra nota");
+//const notaTres = prompt("Ingrese otra nota");
+
+//const notaSuma = parseFloat(notaUno) + parseFloat(notaDos) + parseFloat(notaTres);
+
+//const notaPromedio = notaSuma /3;
+
+//alert("Su promedio es de " + notaPromedio);
+
+
+//Ejercicio 5
+//const medidaInicialMetros = parseFloat(prompt("Por favor ingrese una medida en METROS"));
+
+//const medidaEnPies = medidaInicialMetros * 3.28;
+//const medidaEnCentimetros = medidaInicialMetros * 100;
+//const medidaEnPulgadas = medidaInicialMetros * 39.3701;
+
+//alert("la medida en pies es: " + medidaEnPies);
+//alert("la medida en pies es: " + medidaEnCentimetros);
+//alert("la medida en pies es: " + medidaEnPulgadas);
+
+//Ejercicio 6
+
+//const primerNumero = parseFloat(prompt("ingrese un numero"));
+//const segundoNumero = parseFloat(prompt("ingrese otro numero"));
+
+//const sumaNumeros = primerNumero + segundoNumero;
+
+//alert("El resultado de la suma es: " + sumaNumeros);
+
+//const tercerNumero = parseFloat(prompt("Ingrese un numero para multiplicar"));
+//const multiplicarNumeros = sumaNumeros * tercerNumero;
+
+//alert("El resultado de su multiplicación es: " + multiplicarNumeros);
+
+//Ejercicio 7
+//const minutos = parseFloat(prompt("Ingrese los minutos que desea convertir"));
+//const segundos = minutos * 60;
+
+//alert(minutos + " minutos es igual a: " + segundos);
+
+
+//Ejercicio 8
+//const baseRectangulo = parseFloat(prompt("ingrese la medida de la base"));
+//const alturaRectangulo = parseFloat(prompt("Ingrese la medida de la altura"));
+
+//const area = baseRectangulo * alturaRectangulo;
+//alert("El area total es: " + area);
+
+
+
+//Ejercio 9
+//const base = parseFloat(prompt("Ingrese la medida de la base"));
+//const altura = parseFloat(prompt("Ingrese la medida de la altura"));
+//const area = base * altura /2;
+
+//alert("El area total es: " + area);
+
+
+//Ejercicio 10
+//const monto = parseFloat(prompt("Ingrese el monto"));
+//const descuento = parseFloat(prompt("Ingrese el PORCENTAJE de descuento"));
+//const porcentaje = (100 - descuento) / 100;
+
+//const totalAPagar = monto * porcentaje;
+//alert("El total a pagar es: " + totalAPagar);
+
+
